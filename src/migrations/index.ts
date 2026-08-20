b79b7e1fcf8a931b04b301baf1fb8b2f66323b98
Şaban Durali <sabandurali@gmail.com>
2026-08-20 07:01:27 +0300

    fix: migrate versioned focus area icons

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20260731_034937_initial_postgres_schema from './20260731_034937_initial_postgres_schema';
 import * as migration_20260807_205132_day19_photography from './20260807_205132_day19_photography';
 import * as migration_20260820_090000_day21_focus_area_icons from './20260820_090000_day21_focus_area_icons';
+import * as migration_20260820_100000_day21_versioned_focus_area_icons from './20260820_100000_day21_versioned_focus_area_icons';
 
 export const migrations = [
   {
@@ -17,5 +18,10 @@ export const migrations = [
     up: migration_20260820_090000_day21_focus_area_icons.up,
     down: migration_20260820_090000_day21_focus_area_icons.down,
     name: '20260820_090000_day21_focus_area_icons'
+  },
+  {
+    up: migration_20260820_100000_day21_versioned_focus_area_icons.up,
+    down: migration_20260820_100000_day21_versioned_focus_area_icons.down,
+    name: '20260820_100000_day21_versioned_focus_area_icons'
   },
 ];
